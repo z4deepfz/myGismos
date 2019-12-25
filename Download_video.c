19abd11ddab24a3a36d23043bd745540f8b738f1
z4deepfz <59228892+z4deepfz@users.noreply.github.com>
2019-12-25 23:00:58 +0800

    Create Download_video.c
    用来下载网页视频，可以下载m3u8格式的blob

---
+++ Download_video.c
@@ -0,0 +1,24 @@
+//Windows Only
+//Please install ffmpeg and set correct environment variables
+//Does anyone provide multithreading support? :)
+
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+#define stred(x) x+strlen(x)
+const int MAXL = 512;
+
+int main()
+{
+    char command[MAXL<<2] = "ffmpeg -threads 4 -i ";
+    printf(".m3u8 Address: ");
+    fgets(stred(command), MAXL, stdin);
+    printf("File name: ");
+    memcpy(stred(command)-1, " ", sizeof(char));
+    fgets(stred(command), MAXL, stdin);
+    puts(command);
+    system(command);
+    system("color 27");
+    system("pause");
+    return 0;
+}
